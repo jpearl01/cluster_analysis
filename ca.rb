c6df51c245743dd33e517b8acc0105ea0b5d26e7
josh <joshearl1@hotmail.com>
2013-02-26 15:22:32 -0500

    Moved class defs to before code, got rid of unneeded boolean is_cluster, check to see if we should be adding genes

--- ca.rb
+++ ca.rb
@@ -13,31 +13,39 @@ require 'bio'
 #files for the annotations of each genome.
 #################################
 
-#Read in the clusterGenes file
-
-is_new_cluster   = 0
-is_gene_seqs     = 0
-is_presence_list = 0
-
-File.opt(ARGV[0], "r") do |f|
-  while (line = f.gets)
-    if line.match(<cluster start>\s+(\S+))
-      
-    end
-  
-  end    
-end
-
+#Define the Cluster and Gene classes
 
 class Cluster
-
   attr_accessor :gene_list, :presence_list, :name
-
 end
 
 
 class Gene
-
   attr_accessor :sequence, :name, :product, :contig, :start, :end, :organism, :strain
-
 end
+
+#Read in the clusterGenes file
+
+is_gene_seqs     = false
+is_presence_list = false
+all_clusters     = Array.new
+
+File.open(ARGV[0], "r") do |f|
+  while (line = f.gets)
+    if line.match('<cluster start>\s+(\S+)')
+      #Initialize the cluster object
+      clust = Cluster.new
+      clust.name=$1
+      clust.gene_list = Array.new
+      clust.presence_list = Hash.new
+      all_clusters.push(clust)
+    end
+
+    #We are in the part of the clusterGenes file that lists the different included genes fasta style
+    is_gene_seq = true if line.match('<genes start>')
+    is_gene_seq = false if line.match('<genes stop>')
+
+  end  
+end
+
+puts all_clusters.size
