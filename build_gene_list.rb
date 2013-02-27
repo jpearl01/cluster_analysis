2363d739b0e15059f194604a1070bcd74e0a632a
Josh Earl <joshearl1@hotmail.com>
2013-02-27 00:46:01 -0500

    Added most of the machinery to parse out data from the gene annotation records.  Gene class seems to be working well

--- build_gene_list.rb
+++ build_gene_list.rb
@@ -13,3 +13,24 @@ Dir.foreach('/home/josh/Downloads/annotations/') do |f|
 end
 ex_file = '/home/josh/Downloads/annotations/B502.fasta'
 
+fh = File.open(ex_file, 'r')
+
+genome_name = File.basename(ex_file, '.*')
+
+fh.each_line do |l|
+  
+  if l.match('>')
+    next if l.match(' Contig ')
+    gene = Gene.new
+    l.match('>([\S]+)')
+    gene.name = $1
+    gene.contig = gene.name.split('_')[0]
+    gene.name.gsub!(gene.contig, genome_name)
+    puts gene.name
+    l.match('(product)=[\'\"]{2}(.+)[\'\"]{2}')
+    gene.product = $2
+    puts gene.product
+  end
+
+end
+
